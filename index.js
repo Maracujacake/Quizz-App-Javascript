949cef347a9b1d693b57a6916ac8f35ee718b097
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-08-07 12:25:31 -0300

    jajs

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-waiting
+const startButton = document.getElementById('start-btn')
+const questionContainer = document.getElementById('question-container')
+const embaralharQuestoes, currentQuestionIndex
+
+startButton.addEventListener('click', iniciarJogo)
+
+
+
+function iniciarJogo(){
+    startButton.classList.add('hide')
+    embaralharQuestoes = questions.sort(() => Math.random() - .5)
+    questionContainer.classList.remove('hide')
+    IrProximaQuestao()
+}
+
+function IrProximaQuestao(){
+
+}
+
+function SelecionarResposta(){
+
+}
+
+const = questions [
+    {
+        question: 'Quanto é 2+2?'
+        answers: [
+            {text: '4', correct: true},
+            {text: '20', correct: false},
+            {text: '12', correct: false},
+            {text: '6', correct: false};
+        ]
+    }
+]
